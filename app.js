5c6bc4685f860b882b6b65966520aa39abde7558
Uygar Eren <komifb@hotmail.com>
2024-10-27 13:21:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Running 1!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} üzerinde çalışıyor.`);
+});
