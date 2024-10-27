53d95445f70581501884e9f630d346bef0cf9ec0
uygareren <komifb90@hotmail.com>
2024-10-27 16:33:46 +0300

    auth router added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const authRouter = require("./router/Auth");
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Running 12 3!");
